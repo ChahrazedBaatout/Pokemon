853d7492f65f5029e76ce131524c786ba0402fc2
HADJ SASSI Mahdi <mahdi.hadjsassi@digitalseeder.com>
2025-09-12 14:58:29 +0200

    review PokemonVector class

--- include/PokemonVector.hpp
+++ include/PokemonVector.hpp
@@ -7,18 +7,22 @@ using namespace std;
 using std::vector;
 
 class PokemonVector {
-
 private:
     vector<Pokemon> pokemons;
 
 protected:
-    void addPokemon(const Pokemon& pokemon) ;
-    const Pokemon& getPokemonAt(int index) const ;
-    int getPokemonsCount() const ;
-    const vector<Pokemon>& getPokemons() const ;
+    void addPokemon(const Pokemon &pokemon);
+
+    const Pokemon &getPokemonAt(int index) const;
+
+    int getPokemonsCount() const;
+
+    const vector<Pokemon> &getPokemons() const;
 
 public:
-    Pokemon* getPokemonByIndex(int index) ;
-    Pokemon* getPokemonByName(string name);
-    virtual void displayAllPokemons() ;
+    Pokemon *getPokemonByIndex(int index);
+
+    Pokemon *getPokemonByName(string name);
+
+    virtual void displayAllPokemons();
 };
