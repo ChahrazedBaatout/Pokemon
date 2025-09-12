2712118306944303309988729fd4d5eb48918cd0
HADJ SASSI Mahdi <mahdi.hadjsassi@digitalseeder.com>
2025-09-12 14:58:44 +0200

    review Pokedex class

--- include/Pokedex.cpp
+++ include/Pokedex.cpp
@@ -4,9 +4,9 @@
 #include <string>
 using namespace std;
 
-Pokedex* Pokedex::instance = nullptr;
+Pokedex *Pokedex::instance = nullptr;
 
-Pokedex* Pokedex::getInstance() {
+Pokedex *Pokedex::getInstance() {
     if (instance == nullptr) {
         instance = new Pokedex();
     }
@@ -25,15 +25,15 @@ Pokedex::Pokedex() {
         }
         if (!champs.empty()) {
             addPokemon(Pokemon(stoi(champs[0]), champs[1], stod(champs[5]),
-                              stod(champs[6]), stod(champs[7]), stoi(champs[11]),
-                              stoi(champs[4])));
+                               stod(champs[6]), stod(champs[7]), stoi(champs[11]),
+                               stoi(champs[4])));
         }
     }
 }
 
-void PokemonVector::displayAllPokemons()  {
+void PokemonVector::displayAllPokemons() {
     cout << "All Pokemons in the Pokedex:" << endl;
-    for (Pokemon &pokemon : pokemons) {
+    for (Pokemon &pokemon: pokemons) {
         pokemon.displayInfo();
     }
     cout << "-----------------------" << endl;
--- include/Pokedex.hpp
+++ include/Pokedex.hpp
@@ -1,14 +1,16 @@
 #include "PokemonVector.hpp"
 #include <string>
-using namespace  std;
+using namespace std;
 
 class Pokedex : public PokemonVector {
 private:
-    static Pokedex* instance;
-    string chemin = "pokedex.csv";
+    static Pokedex *instance;
+    string chemin = "pokedex.csv"; // I suggest this to be a constant or enviromental variable
+
     Pokedex();
 
 public:
-    static Pokedex* getInstance();
-
+    static Pokedex *getInstance();
+    // Since Pokedex inherits from PokemonVector, it should override the virtual method : displayAllPokemons()
+    void displayAllPokemons() override;
 };
