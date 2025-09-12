3e7df2f40a952e764bcc6fa396326ab0d2396391
HADJ SASSI Mahdi <mahdi.hadjsassi@digitalseeder.com>
2025-09-12 14:58:12 +0200

    review Pokemon class

--- include/Pokemon.cpp
+++ include/Pokemon.cpp
@@ -1,10 +1,11 @@
 #include "../include/Pokemon.hpp"
 #include <iostream>
 #include <string>
-using namespace  std;
+using namespace std;
 
 
-Pokemon::Pokemon(int id, string name, int evolution, double maxHitPoint, double currHitPoint, double attack, double defense)
+Pokemon::Pokemon(int id, string name, int evolution, double maxHitPoint, double currHitPoint, double attack,
+                 double defense)
     : id(id),
       name(name),
       evolution(evolution),
@@ -14,7 +15,7 @@ Pokemon::Pokemon(int id, string name, int evolution, double maxHitPoint, double 
       defense(defense) {
 }
 
-Pokemon::Pokemon(const Pokemon& other)
+Pokemon::Pokemon(const Pokemon &other)
     : id(other.id),
       name(other.name),
       evolution(other.evolution),
@@ -23,15 +24,17 @@ Pokemon::Pokemon(const Pokemon& other)
       attack(other.attack),
       defense(other.defense) {
     cout << "Constructeur de copie appelé" << endl;
-    cout << endl;
+    //this is good for testing, but i rather use debugging tools, than just putting a console printing.
+    cout << endl; // I don't understand why you put this line
 }
 
 Pokemon::~Pokemon() {
+    // I think if the destructor is not doing anything special just showing a message, we can just remove it
     cout << "Destructeur appelé: " << name << " détruit" << endl;
     cout << endl;
 }
 
-// Getters
+// Getters // to remove this comment, since it's obvious that these are getters
 int Pokemon::getId() const { return id; }
 string Pokemon::getName() const { return name; }
 int Pokemon::getEvolution() const { return evolution; }
@@ -39,6 +42,7 @@ double Pokemon::getMaxHitPoint() const { return maxHitPoint; }
 double Pokemon::getCurrHitPoint() const { return currHitPoint; }
 double Pokemon::getAttack() const { return attack; }
 double Pokemon::getDefense() const { return defense; }
+
 void Pokemon::displayInfo() {
     cout << "ID: " << id << endl;
     cout << "Nom: " << name << endl;
@@ -50,17 +54,30 @@ void Pokemon::displayInfo() {
     cout << endl;
 }
 
-void Pokemon::attacking(Pokemon& target) const{
+void Pokemon::attacking(Pokemon &target) const {
+    /*
     double damage = this->attack - target.defense ;
     if (damage < 0) damage = 0;
+    //the instruction of damage = 0 is not necessary, we can put all the rest in the condition bloc
 
     target.currHitPoint -= damage;
     if (target.currHitPoint < 0) target.currHitPoint= 0;
+    // There is no need to assign 0 to the currHitPoint if it's less than 0
+    // we can just show the last message in case the currHitPoint is less or equal than 0
 
     cout << this->name << " attaque " << target.name <<endl;
 
 
     if (target.currHitPoint== 0) {
         cout << target.name << " est decede. !" << endl;
+    }
+    */
+    // I suggest this version.
+    double damage = this->attack - target.defense;
+    if (damage > 0) {
+        target.currHitPoint -= damage;
+        if (target.currHitPoint <= 0) {
+            cout << target.name << " est decede. !" << endl;
+        }
     }
 }
--- include/Pokemon.hpp
+++ include/Pokemon.hpp
@@ -1,6 +1,6 @@
-#include<iostream>
+#include<iostream> // This line is unsed since it's imported in the Pokemon.cpp, we can remove it from the code
 #include <string>
-using namespace  std;
+using namespace std;
 
 class Pokemon {
 private:
@@ -14,22 +14,38 @@ private:
 
 public:
     Pokemon(int id, string name, int evolution, double maxHitPoint, double currHitPoint, double attack, double defense);
-    Pokemon(const Pokemon& other);
+
+    Pokemon(const Pokemon &other); // I prefer to use the parameter otherPokemon it would be more comprehensible
+
     ~Pokemon();
-    void attacking(Pokemon& target) const;
+
+    void attacking(Pokemon &target) const;
+
     void displayInfo();
+
     int getId() const;
+
     string getName() const;
+
     int getEvolution() const;
+
     double getMaxHitPoint() const;
+
     double getCurrHitPoint() const;
+
     double getAttack() const;
+
     double getDefense() const;
-    void setName(const string& n);
+
+    void setName(const string &n);
+
     void setEvolution(int evo);
+
     void setMaxHitPoint(double maxhp);
+
     void setCurrHitPoint(double currhp);
+
     void setAttack(double atk);
+
     void setDefense(double def);
 };
-
