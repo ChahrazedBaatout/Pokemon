94782591b9a0fa7ea83808df24f7aeeec6524877
HADJ SASSI Mahdi <mahdi.hadjsassi@digitalseeder.com>
2025-09-12 15:40:31 +0200

    update the main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
+#include "gui/WelcomePage/WelcomePage.hpp"
 #include <SFML/Graphics.hpp>
 int main() {
     sf::RenderWindow window(sf::VideoMode(800, 600), "Hello SFML");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+
+    WelcomePage welcomePage;
+    bool showWelcome = true;
+
+    sf::Font font;
+    font.loadFromFile("../resources/font/arial.ttf");
+    sf::Text mainText("Page principale", font, 48);
+    mainText.setFillColor(sf::Color::Green);
+    mainText.setPosition(180, 250);
 
     while (window.isOpen()) {
         sf::Event event{};
@@ -10,8 +18,19 @@ int main() {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+
         window.clear();
-        window.draw(shape);
+
+        if (showWelcome) {
+            welcomePage.update();
+            window.draw(welcomePage);
+            if (welcomePage.isTimeUp()) {
+                showWelcome = false;
+            }
+        } else {
+            window.draw(mainText);
+        }
+
         window.display();
     }
     return 0;
